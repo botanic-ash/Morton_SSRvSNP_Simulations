5bf339ee8570a502e9194c91b4eaa5ea45e8c14a
akoontz11 <austinkoontz11@gmail.com>
2022-04-04 16:07:23 -0500

    Function to convert all Arlequin files in a directory to a list of genind objects; sense check of number of alleles per sim. scenario (currently failing)

--- samplingOutputs.R
+++ samplingOutputs.R
@@ -1,9 +1,14 @@
-# Parsing fastSimcoal2 Arlequin outputs
+# Parsing fastSimcoal2 outputs
 
 library(adegenet)
 library(hierfstat)
+library(stringr)
+setwd("/home/akoontz/Documents/SSRvSNP/Simulations/Code/")
+
 # Specify /RAID1 directory, where outputs are being held
-setwd("/RAID1/Simulations/Even_20220329_FSCdraft/")
+sim.directory <- "/RAID1/Simulations/Even_20220329_FSCdraft/"
+# Additional directory, for samples simulated using the -s flag (for DNA markers)
+# sim.directory <- "/RAID1/Simulations/SNP_20220404_FSCdraft/"
 
 # Conversion using strataG package----
 library(strataG)
@@ -15,37 +20,94 @@ strataG_arp2gen <- function(arp.path){
   return(genind.object)
 }
 
+# Function for converting all Arlequin files in a specified directory (using above strataG_arp2gen fxn)
+convertAllArp <- function(arp.path){
+  # Retrieve original working directory, to reset to after conversion
+  original.WD <- getwd()
+  # Navigate to the folder containing simulation outputs
+  setwd(arp.path)
+  # Create an empty list object to receive list of genind
+  genind.list <- list(length=length(dir()[str_detect(dir(), pattern = ".arp")]))
+  # Convert all Arlequin files to adegenet, creating a list of genind objects
+  genind.list <- lapply(dir()[str_detect(dir(), pattern = ".arp")], strataG_arp2gen)
+  # Reset to original working directory, and return a list of genind objects
+  setwd(original.WD)
+  return(genind.list)
+}
+
 # Microsatellite files
-MSAT_1pop_migHigh <- strataG_arp2gen("MSAT_01pops_migHigh/MSAT_01pops_migHigh_1_1.arp")
+MSAT_1pop_migLow <- convertAllArp(paste0(sim.directory,"MSAT_01pops_migLow"))
+MSAT_1pop_migHigh <- convertAllArp(paste0(sim.directory,"MSAT_01pops_migHigh"))
 
-MSAT_4pops_migHigh <- strataG_arp2gen("MSAT_04pops_migHigh/MSAT_04pops_migHigh_1_1.arp")
-nInd(MSAT_4pops_migHigh)
+MSAT_4pop_migLow <- convertAllArp(paste0(sim.directory,"MSAT_04pops_migLow"))
+MSAT_4pop_migHigh <- convertAllArp(paste0(sim.directory,"MSAT_04pops_migHigh"))
 
-MSAT_16pops_migHigh <- strataG_arp2gen("MSAT_16pops_migHigh/MSAT_16pops_migHigh_1_1.arp")
-nInd(MSAT_16pops_migHigh)
+MSAT_16pop_migLow <- convertAllArp(paste0(sim.directory,"MSAT_16pops_migLow"))
+MSAT_16pop_migHigh <- convertAllArp(paste0(sim.directory,"MSAT_16pops_migHigh"))
 
 # DNA files
-DNA_1pop_migHigh <- strataG_arp2gen("DNA_01pops_migHigh/DNA_01pops_migHigh_1_1.arp")
-nInd(DNA_1pop_migHigh)
+DNA_1pop_migLow <- convertAllArp(paste0(sim.directory,"DNA_01pops_migLow"))
+DNA_1pop_migHigh <- convertAllArp(paste0(sim.directory,"DNA_01pops_migHigh"))
 
-DNA_4pops_migHigh <- strataG_arp2gen("DNA_04pops_migHigh/DNA_04pops_migHigh_1_1.arp")
-nInd(DNA_4pops_migHigh)
+DNA_4pop_migLow <- convertAllArp(paste0(sim.directory,"DNA_04pops_migLow"))
+DNA_4pop_migHigh <- convertAllArp(paste0(sim.directory,"DNA_04pops_migHigh"))
 
-DNA_16pops_migHigh <- strataG_arp2gen("DNA_16pops_migHigh/DNA_16pops_migHigh_1_1.arp")
-nInd(DNA_16pops_migHigh)
+DNA_16pop_migLow <- convertAllArp(paste0(sim.directory,"DNA_16pops_migLow"))
+DNA_16pop_migHigh <- convertAllArp(paste0(sim.directory,"DNA_16pops_migHigh"))
 
-# OUTDATED: Conversion using diveRsity package----
-library(diveRsity)
+# Sense check----
+# 1. More alleles in scenarios with a larger number of populations
+# Exploring what DNA alleles matrices look like
+str(DNA_1pop_migLow[[1]]@tab)
+DNA_1pop_migLow[[1]]@tab[1:5,1:5]
 
-# DNA Markers
-arp2gen("DNA_01pops_migLow/DNA_01pops_migLow_1_1.arp")
-# Error: "Data are not in 'MICROSAT' format!"
+str(DNA_4pop_migLow[[1]]@tab)
+DNA_4pop_migLow[[1]]@tab[1:5,1:5]
 
-# MSAT Markers
-# 1 population 
-arp2gen("MSAT_01pops_migLow/MSAT_01pops_migLow_1_1.arp")
-# Error: "Error in substr(dat[sampSizeLine], start = sampNpos + 1, stop = nchar(dat[sampSizeLine])): object 'sampNpos' not found"
+unique(rowSums(DNA_1pop_migLow[[1]]@tab))
+length(which(DNA_1pop_migLow[[1]]@tab[1,] == 0))
+length(which(DNA_1pop_migLow[[1]]@tab[2,] == 0))
+
+length(which(DNA_1pop_migLow[[1]]@tab[,1] == 0))
+length(which(DNA_1pop_migLow[[1]]@tab[,2] == 0))
+
+# Microsatellites
+# Number of alleles for each simulation instance
+lapply(MSAT_1pop_migLow, FUN = function(x) ncol(x@tab))
+# Average
+mean(as.numeric(lapply(MSAT_1pop_migLow, FUN = function(x) ncol(x@tab))))
 # 4 populations
-arp2gen("MSAT_04pops_migLow/MSAT_04pops_migLow_1_1.arp")
-MSAT_1pop_migHigh_diveRsity <- read.genepop("MSAT_04pops_migLow/MSAT_04pops_migLow_1_1.gen", ncode=3)
-nInd(MSAT_1pop_migHigh_diveRsity)
+lapply(MSAT_4pop_migLow, FUN = function(x) ncol(x@tab))
+mean(as.numeric(lapply(MSAT_4pop_migLow, FUN = function(x) ncol(x@tab))))
+# 16 populations
+lapply(MSAT_16pop_migLow, FUN = function(x) ncol(x@tab))
+mean(as.numeric(lapply(MSAT_16pop_migLow, FUN = function(x) ncol(x@tab))))
+
+mean(as.numeric(lapply(MSAT_1pop_migHigh, FUN = function(x) ncol(x@tab))))
+mean(as.numeric(lapply(MSAT_4pop_migHigh, FUN = function(x) ncol(x@tab))))
+mean(as.numeric(lapply(MSAT_16pop_migHigh, FUN = function(x) ncol(x@tab))))
+
+# DNA
+# Number of alleles for each simulation instance
+lapply(DNA_1pop_migLow, FUN = function(x) ncol(x@tab))
+lapply(DNA_4pop_migLow, FUN = function(x) ncol(x@tab))
+lapply(DNA_16pop_migLow, FUN = function(x) ncol(x@tab))
+lapply(DNA_16pop_migHigh, FUN = function(x) ncol(x@tab))
+
+# 2. Lower Fst for scenarios with lower migration rates
+
+# # OUTDATED: Conversion using diveRsity package----
+# library(diveRsity)
+# 
+# # DNA Markers
+# arp2gen("DNA_01pops_migLow/DNA_01pops_migLow_1_1.arp")
+# # Error: "Data are not in 'MICROSAT' format!"
+# 
+# # MSAT Markers
+# # 1 population 
+# arp2gen("MSAT_01pops_migLow/MSAT_01pops_migLow_1_1.arp")
+# # Error: "Error in substr(dat[sampSizeLine], start = sampNpos + 1, stop = nchar(dat[sampSizeLine])): object 'sampNpos' not found"
+# # 4 populations
+# arp2gen("MSAT_04pops_migLow/MSAT_04pops_migLow_1_1.arp")
+# MSAT_1pop_migHigh_diveRsity <- read.genepop("MSAT_04pops_migLow/MSAT_04pops_migLow_1_1.gen", ncode=3)
+# nInd(MSAT_1pop_migHigh_diveRsity)
