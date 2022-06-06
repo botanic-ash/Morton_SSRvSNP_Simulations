5fb5a6696ce44f9fc6087ac086093944e912a3ea
akoontz11 <austinkoontz11@gmail.com>
2022-06-06 10:39:54 -0500

    Comments

--- fscParams/DNAmarker_Demo.R
+++ fscParams/DNAmarker_Demo.R
@@ -3,11 +3,12 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # This script uses strataG to create the fastsimcoal2 (fsc) parameter files with DNA markers
-# It then demonstrates the issue of single loci, using these DNA markers
+# It then demonstrates the issue of single loci, using these DNA markers (in ANALYZING OUTPUTS)
 # Multiple loci appear in the DNAmarker_Demo object, but only 1 in the DNAmarker_Demo_genind object
 
 library(strataG)
 library(adegenet)
+# Set below line to whatever working directory is necessary
 setwd("~/Documents/SSRvSNP/Simulations/Code/fscParams/")
 
 # ----VARIABLES----
@@ -17,9 +18,10 @@ fscVersion <- "fsc2702"
 demeA <- fscDeme(deme.size = 10, sample.size = 10)
 # DNA Genetic parameters
 # This should generate 8 blocks (4 loci, for a diploid individual) of DNA sequences of length 15 bp
-dna <- fscBlock_dna(sequence.length = 15, mut.rate = 1e-4)
+dna <- fscBlock_dna(sequence.length = 15, mut.rate = 1e-3)
 DNAgenetics <- fscSettingsGenetics(dna, dna, dna, num.chrom = 1)
 
+# ----RUNNING FASTSIMCOAL2----
 # Generate a parameter file
 DNAmarker_Demo.params <- fscWrite(demes = fscSettingsDemes(demeA), genetics = DNAgenetics, 
                                   label = "DNAmarker_Demo", use.wd=TRUE)
@@ -30,6 +32,7 @@ DNAmarker_Demo.params <- fscRun(DNAmarker_Demo.params, num.sims = num_reps, all.
 DNAmarker_Demo <- fscReadArp(DNAmarker_Demo.params, sim = c(1,1), marker = "dna")
 dim(DNAmarker_Demo)
 
+# ----ANALYZING OUTPUTS----
 # Can't convert fsc params to gtype: "Error: the number of genes in 'sequences' is not equal to the number of loci"
 DNAmarker_Demo_gtype <- fsc2gtypes(DNAmarker_Demo.params,marker = "dna")
 
