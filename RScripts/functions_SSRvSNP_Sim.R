d881e62b2cf3851fcbbde75fd111c139fc185ac4
akoontz11 <austinkoontz11@gmail.com>
2022-08-18 17:44:56 -0500

    Added assignGarden and exSituRepresentation functions

--- RScripts/functions_SSRvSNP_Sim.R
+++ RScripts/functions_SSRvSNP_Sim.R
@@ -44,3 +44,42 @@ convertAllArp <- function(arp.path, params){
   setwd(original.WD)
   return(genind.list)
 }
+
+# Function for randomly assigning a proportion of a genind matrix to a "garden" population (the rest get "wild")
+assignGardenSamples <- function(genind.obj, proportion=0.2){
+  # Create a vector to store population names (start with all samples being "wild")
+  popIDs <- rep("wild",length=(nInd(genind.test)))
+  # Get the names of randomly sampled rows of the genind matrix, based on the proportion argument
+  gardenSamples <- rownames(genind.obj@tab[sample(nrow(genind.obj@tab), 
+                                                  size=nInd(genind.obj)*proportion, replace = FALSE),])
+  # Assign randomly sampled rows as "garden"
+  popIDs[which(rownames(genind.test@tab) %in% gardenSamples)] <- "garden"
+  # Assign pop values and return genind object
+  pop(genind.obj) <- popIDs
+  return(genind.obj)
+}
+
+# Function for reporting ex situ representation rates, using a single genind object
+exSituRepresentation <- function(gen.obj){
+  # Generate numerical vectors corresponding to garden and wild rows
+  garden.Rows <- which(pop(gen.obj)=="garden")
+  wild.Rows <- which(pop(gen.obj)!="garden")
+  # Build the wild allele frequency vector
+  wildFreqs <- colSums(gen.obj@tab[wild.Rows,], na.rm = TRUE)/(length(wild.Rows)*2)*100
+  # Calculate representation rates
+  # Total
+  total <- length(which(names(which(wildFreqs > 0)) %in% names(which(colSums(gen.obj@tab[garden.Rows,], na.rm = TRUE) > 0))))/length(which(wildFreqs > 0))*100
+  # Very common
+  veryCommon <- length(which(names(which(wildFreqs > 10)) %in% names(which(colSums(gen.obj@tab[garden.Rows,], na.rm = TRUE) > 0))))/length(which(wildFreqs > 10))*100
+  # Common
+  common <- length(which(names(which(wildFreqs > 5)) %in% names(which(colSums(gen.obj@tab[garden.Rows,], na.rm = TRUE) > 0))))/length(which(wildFreqs > 5))*100
+  # Low frequency
+  lowFrequency <- length(which(names(which(wildFreqs < 10 & wildFreqs > 1)) %in% names(which(colSums(gen.obj@tab[garden.Rows,], na.rm = TRUE) > 0))))/length(which(wildFreqs < 10 & wildFreqs > 1))*100
+  # Rare
+  rare <- length(which(names(which(wildFreqs < 1 & wildFreqs > 0)) %in% names(which(colSums(gen.obj@tab[garden.Rows,], na.rm = TRUE) > 0))))/length(which(wildFreqs < 1 & wildFreqs > 0))*100
+  # Build list of rates
+  repRates <- c(total,veryCommon,common,lowFrequency,rare)
+  names(repRates) <- c("Total","Very common (>10%)","Common (>5%)","Low frequency (1% -- 10%)","Rare (<1%)")
+  # Print representation rates and return
+  return(repRates)
+}
