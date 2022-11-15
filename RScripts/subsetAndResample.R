f5610beb5dbe4641192a5b4bcdc62ef1d12ec6a4
akoontz11 <austinkoontz11@gmail.com>
2022-11-15 11:29:17 -0600

    Changed working directory to shared folder location; trimmed this script to only contain 2 scenarios (1 MSAT, 1 DNA)

--- RScripts/subsetAndResample.R
+++ RScripts/subsetAndResample.R
@@ -12,8 +12,9 @@ library(adegenet)
 library(stringr)
 library(hierfstat)
 
-# Set working directory to local GitHub repository 
-sim.wd <- "/RAID1/Simulations/Code/"
+# Set working directory to the GitHub repo (containing scripts and fastSimcoal outputs;
+# this is a file located on the RAID1 drive, for space reasons, and linked in the home directory)
+sim.wd <- "~/Shared/SSRvSNP_Sim/Code/"
 setwd(sim.wd)
 # Read in relevant functions
 source("RScripts/functions_SSRvSNP_Sim.R")
@@ -22,53 +23,23 @@ source("RScripts/functions_SSRvSNP_Sim.R")
 # Alternatively, source the genind objects from previously run simulations, using readGeninds functions
 # Microsatellites
 readGeninds_MSAT(paste0(sim.wd,"SimulationOutputs/MSAT_marker/data.MSAT/"))
-readGeninds_DNA(paste0(sim.wd,"SimulationOutputs/DNA_medMut_marker/data.DNA/"), prefix="DNA_medMut")
+# readGeninds_DNA(paste0(sim.wd,"SimulationOutputs/DNA_medMut_marker/data.DNA/"), prefix="DNA_medMut")
 readGeninds_DNA(paste0(sim.wd,"SimulationOutputs/DNA_marker/data.DNA/"))
-# DNA, medium-high mutation (5e-5)
-# readGeninds_DNA(paste0(sim.wd,"SimulationOutputs/DNA_medHighMut_marker/data.DNA/"), prefix="DNA_medHighMut")
 
 # Specify the proportion of individuals assigned to gardens
 gardenRate <- 0.05
 # Make list of genind objects
 MSAT_geninds <- list(MSAT_01pop_migLow.genind, MSAT_01pop_migHigh.genind, MSAT_04pop_migLow.genind,
                      MSAT_04pop_migHigh.genind, MSAT_16pop_migLow.genind, MSAT_16pop_migHigh.genind)
-# DNA_geninds <- list(DNA_01pop_migLow.genind, DNA_01pop_migHigh.genind, DNA_04pop_migLow.genind,
-#                     DNA_04pop_migHigh.genind, DNA_16pop_migLow.genind, DNA_16pop_migHigh.genind)
-# DNA_highMut_geninds <- list(DNA_highMut_01pop_migLow.genind, DNA_highMut_01pop_migHigh.genind, DNA_highMut_04pop_migLow.genind,
-#                     DNA_highMut_04pop_migHigh.genind, DNA_highMut_16pop_migLow.genind, DNA_highMut_16pop_migHigh.genind)
-DNA_medMut_geninds <- list(DNA_medMut_01pop_migLow.genind, DNA_medMut_01pop_migHigh.genind, DNA_medMut_04pop_migLow.genind,
-                            DNA_medMut_04pop_migHigh.genind, DNA_medMut_16pop_migLow.genind, DNA_medMut_16pop_migHigh.genind)
 DNA_geninds <- list(DNA_01pop_migLow.genind, DNA_01pop_migHigh.genind, DNA_04pop_migLow.genind,
                            DNA_04pop_migHigh.genind, DNA_16pop_migLow.genind, DNA_16pop_migHigh.genind)
-# DNA_medHighMut_geninds <- list(DNA_medHighMut_01pop_migLow.genind, DNA_medHighMut_01pop_migHigh.genind, DNA_medHighMut_04pop_migLow.genind,
-#                            DNA_medHighMut_04pop_migHigh.genind, DNA_medHighMut_16pop_migLow.genind, DNA_medHighMut_16pop_migHigh.genind)
 
 # %%% MSAT ----
-# Range.constraint=15
 summarize_simulations(MSAT_geninds, gardenRate = gardenRate)
 
-# Range.constraint=10
-# summarize_simulations(MSAT_rang10_geninds, gardenRate = gardenRate)
-
-# Range.constraint=10, higher mutation rate (1e-3) ----
-# summarize_simulations(MSAT_highMut_geninds, gardenRate = gardenRate)
-
 # %%% DNA ----
-# Lower mutation (1e-8)
-# summarize_simulations(DNA_geninds, gardenRate = gardenRate)
-
-# Higher mutation (1e-5)
-# summarize_simulations(DNA_highMut_geninds, gardenRate = gardenRate)
-
-# Medium mutation (1e-6)
-summarize_simulations(DNA_medMut_geninds, gardenRate = gardenRate)
-
-# Larger numbers of SNP loci
 summarize_simulations(DNA_geninds, gardenRate = gardenRate)
 makeAlleleFreqHist(DNA_geninds[[4]][[3]], title="Simulated DNA: 1,000 loci, mutation.rate=5e-6 (4 pops, high migration)")
-
-# Medium-high mutation (5e-6)
-# summarize_simulations(DNA_medHighMut_geninds, gardenRate = gardenRate)
 
 # !!! WORKING !!!
 # %%% RESAMPLING %%% ----
