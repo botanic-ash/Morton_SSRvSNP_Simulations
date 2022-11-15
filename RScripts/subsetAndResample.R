be369667f9511993d2af23f1507c7b66f393f429
akoontz11 <austinkoontz11@gmail.com>
2022-11-15 16:12:21 -0600

    Included parallelized resampling commands

--- RScripts/subsetAndResample.R
+++ RScripts/subsetAndResample.R
@@ -11,6 +11,7 @@ library(strataG)
 library(adegenet)
 library(stringr)
 library(hierfstat)
+library(parallel)
 
 # Set working directory to the GitHub repo (containing scripts and fastSimcoal outputs;
 # this is a file located on the RAID1 drive, for space reasons, and linked in the home directory)
@@ -18,7 +19,13 @@ sim.wd <- "~/Shared/SSRvSNP_Sim/Code/"
 setwd(sim.wd)
 # Read in relevant functions
 source("RScripts/functions_SSRvSNP_Sim.R")
-# Run simulations
+# Parallelism
+# Set up relevant cores, and make sure adegenet and parallel libraries are present on the cluster
+num_cores <- detectCores() - 1 ; cl <- makeCluster(num_cores)
+clusterEvalQ(cl, library("adegenet"))
+clusterEvalQ(cl, library("parallel"))
+
+# %%% READ IN SIMULATIONS AND PROCESS RESULTS %%% ----
 # source("RScripts/GenerateFSCparams.R")
 # Alternatively, source the genind objects from previously run simulations, using readGeninds functions
 # Microsatellites
@@ -33,33 +40,48 @@ MSAT_geninds <- list(MSAT_01pop_migLow.genind, MSAT_01pop_migHigh.genind, MSAT_0
                      MSAT_04pop_migHigh.genind, MSAT_16pop_migLow.genind, MSAT_16pop_migHigh.genind)
 DNA_geninds <- list(DNA_01pop_migLow.genind, DNA_01pop_migHigh.genind, DNA_04pop_migLow.genind,
                            DNA_04pop_migHigh.genind, DNA_16pop_migLow.genind, DNA_16pop_migHigh.genind)
-
-# %%% MSAT ----
+# Simulation summaries
+# MSAT
 summarize_simulations(MSAT_geninds, gardenRate = gardenRate)
-
-# %%% DNA ----
+# DNA
 summarize_simulations(DNA_geninds, gardenRate = gardenRate)
 makeAlleleFreqHist(DNA_geninds[[4]][[3]], title="Simulated DNA: 1,000 loci, mutation.rate=5e-6 (4 pops, high migration)")
 
-# !!! WORKING !!!
 # %%% RESAMPLING %%% ----
-# Set up relevant cores, and make sure adegenet library is present on cluster
-num_cores <- detectCores() - 1 ; cl <- makeCluster(num_cores)
-clusterEvalQ(cl, library("adegenet"))
-
-# MSAT ----
-# Specify number of replicates
-num_reps <- 50
+# Specify number of replicates, to use for both marker types
+num_reps <- 5
 # Export relevant functions and variables
-clusterExport(cl, varlist = c("getAlleleCategories", "exSitu_Sample", "exSitu_Resample", 
-                              "num_reps", "MSAT_geninds"))
-# Run resampling in parallel, to generate an array
-samplingResults_MSAT.01pop.migLow.1 <- 
-  parSapply(cl, 1:num_reps, 
-            function(a) exSitu_Resample(gen.obj = MSAT_01pop_migLow.genind[[1]]), 
-            simplify = "array")
+clusterExport(cl, varlist = c("getWildFreqs", "getAlleleCategories", "exSitu_Sample", 
+                              "exSitu_Resample", "parResample_genind", "num_reps", "gardenRate",
+                              "MSAT_geninds", "DNA_geninds"))
 
+# MSAT 
+MSAT_01pop_migLow.Resampling <- lapply(MSAT_01pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+MSAT_01pop_migHigh.Resampling <- lapply(MSAT_01pop_migHigh.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+MSAT_04pop_migLow.Resampling <- lapply(MSAT_04pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+MSAT_04pop_migHigh.Resampling <- lapply(MSAT_04pop_migHigh.genind, 
+                                        function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+MSAT_16pop_migLow.Resampling <- lapply(MSAT_16pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+MSAT_16pop_migHigh.Resampling <- lapply(MSAT_16pop_migHigh.genind, 
+                                        function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
 
+# DNA
+DNA_01pop_migLow.Resampling <- lapply(DNA_01pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+DNA_01pop_migHigh.Resampling <- lapply(DNA_01pop_migHigh.genind, 
+                                        function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+DNA_04pop_migLow.Resampling <- lapply(DNA_04pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+DNA_04pop_migHigh.Resampling <- lapply(DNA_04pop_migHigh.genind, 
+                                        function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+DNA_16pop_migLow.Resampling <- lapply(DNA_16pop_migLow.genind, 
+                                       function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
+DNA_16pop_migHigh.Resampling <- lapply(DNA_16pop_migHigh.genind, 
+                                        function(x) parResample_genind(x, gRate=gardenRate, cluster=cl))
 
 # Close cores
 stopCluster(cl)
