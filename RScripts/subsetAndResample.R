57ac56a382b41f5831c6cd4cd9a70394929a0888
akoontz11 <austinkoontz11@gmail.com>
2022-12-14 11:23:21 -0600

    Added code for analyzying _N4800 datasets

--- RScripts/subsetAndResample.R
+++ RScripts/subsetAndResample.R
@@ -27,11 +27,12 @@ setwd(sim.wd)
 # Read in relevant functions
 source("RScripts/functions_SSRvSNP_Sim.R")
 # Parallelism: set up relevant cores; load adegenet and parallel libraries onto the cluster
-num_cores <- detectCores() - 1 ; cl <- makeCluster(num_cores)
+num_cores <- detectCores() - 16 ; cl <- makeCluster(num_cores)
 clusterEvalQ(cl, library("adegenet"))
 clusterEvalQ(cl, library("parallel"))
 
-# %%% READ IN SIMULATIONS AND PROCESS RESULTS %%% ----
+# %%% ORIGINAL SIMULATIONS: NIND 1200 %%% ----
+# %%% Read in simulations and process results ----
 # Run the simulations
 # source("RScripts/GenerateFSCparams.R")
 # Alternatively, source the genind objects from previously run simulations, using readGeninds functions
@@ -44,7 +45,7 @@ MSAT_geninds <- list(MSAT_01pop_migLow.genList, MSAT_01pop_migHigh.genList, MSAT
 DNA_geninds <- list(DNA_01pop_migLow.genList, DNA_01pop_migHigh.genList, DNA_04pop_migLow.genList,
                     DNA_04pop_migHigh.genList, DNA_16pop_migLow.genList, DNA_16pop_migHigh.genList)
 
-# %%% ASSIGN GARDEN SAMPLES %%% ----
+# %%% Assign garden samples ----
 # Specify the proportion of total individuals assigned to gardens
 gardenRate <- 0.05
 # Assign individuals to garden populations. Since we're dealing with a list of lists, we use rapply
@@ -52,7 +53,7 @@ gardenRate <- 0.05
 MSAT_geninds <- rapply(MSAT_geninds, assignGardenSamples, proportion=gardenRate, how = "list")
 DNA_geninds <- rapply(DNA_geninds, assignGardenSamples, proportion=gardenRate, how = "list")
 
-# %%% SUMMARIZE SIMULATIONS %%% ----
+# %%% Summarize simulations ----
 # MSAT
 summarize_simulations(MSAT_geninds)
 # DNA
@@ -61,7 +62,7 @@ summarize_simulations(DNA_geninds)
 makeAlleleFreqHist(DNA_geninds[[4]][[3]], 
                    title="Simulated DNA: 1,000 loci, mutation.rate=5e-6 (4 pops, high migration)")
 
-# %%% RESAMPLING %%% ----
+# %%% Resampling ----
 # Specify number of resampling replicates, to use for both marker types
 num_reps <- 5
 # Export relevant functions and variables to the cluster
@@ -75,9 +76,9 @@ MSAT_resamplingArrays <- rapply(MSAT_geninds, parResample_genind, reps=num_reps,
 DNA_resamplingArrays <- rapply(DNA_geninds, parResample_genind, reps=num_reps, cluster=cl, how = "list")
 
 # Close cores
-stopCluster(cl)
+# stopCluster(cl)
 
-# %%% SUMMARIZE RESAMPLING RESULTS %%% ----
+# %%% Summarize resampling results ----
 # Calculate 95% minimum sampling size, and the mean values of each category, for all scenarios
 # MSAT
 MSAT_min95_Means <- rapply(MSAT_resamplingArrays, resample_min95_mean, how = "list")
@@ -86,7 +87,7 @@ MSAT_meanValues <- rapply(MSAT_resamplingArrays, resample_meanValues, how = "lis
 DNA_min95Values <- rapply(DNA_resamplingArrays, resample_min95_mean, how = "list")
 DNA_meanValues <- rapply(DNA_resamplingArrays, resample_meanValues, how = "list")
 
-# %%% PLOTTING %%% 
+# %%% Plotting ---- 
 # Pick plot colors (for all plots!), with transparency for values other than Total
 plotColors <- c("red","red4","darkorange3","coral","purple")
 
@@ -95,3 +96,71 @@ plotColors <- c("red","red4","darkorange3","coral","purple")
 invisible(rapply(MSAT_resamplingArrays, resample_Plot, colors=plotColors))
 # DNA
 invisible(rapply(DNA_resamplingArrays, resample_Plot, colors=plotColors))
+
+# %%% INCREASED SIMULATIONS: NIND 4800 %%% ----
+# %%% Read in simulations and process results ----
+# Run the simulations
+# source("RScripts/GenerateFSCparams_N4800.R")
+# Alternatively, source the genind objects from previously run simulations, using readGeninds functions
+readGeninds_MSAT(paste0(sim.wd,"SimulationOutputs/MSAT_N4800_marker/data.MSAT/"))
+readGeninds_DNA(paste0(sim.wd,"SimulationOutputs/DNA_N4800_marker/data.DNA/"))
+
+# Combine all scenarios for each marker into a list of genind lists
+MSAT_N4800_geninds <- list(MSAT_N4800_01pop_migLow.genList, MSAT_N4800_01pop_migHigh.genList, MSAT_N4800_04pop_migLow.genList,
+                     MSAT_N4800_04pop_migHigh.genList, MSAT_N4800_16pop_migLow.genList, MSAT_N4800_16pop_migHigh.genList)
+DNA_N4800_geninds <- list(DNA_N4800_01pop_migLow.genList, DNA_N4800_01pop_migHigh.genList, DNA_N4800_04pop_migLow.genList,
+                    DNA_N4800_04pop_migHigh.genList, DNA_N4800_16pop_migLow.genList, DNA_N4800_16pop_migHigh.genList)
+
+# %%% Assign garden samples ----
+# Specify the proportion of total individuals assigned to gardens
+gardenRate <- 0.05
+# Assign individuals to garden populations. Since we're dealing with a list of lists, we use rapply
+# (We could also use a nested lapply: genind_list <- lapply (genind_list, lapply, function, args))
+MSAT_N4800_geninds <- rapply(MSAT_N4800_geninds, assignGardenSamples, proportion=gardenRate, how = "list")
+DNA_N4800_geninds <- rapply(DNA_N4800_geninds, assignGardenSamples, proportion=gardenRate, how = "list")
+
+# %%% Summarize simulations ----
+# MSAT
+summarize_simulations(MSAT_N4800_geninds)
+# DNA
+summarize_simulations(DNA_N4800_geninds)
+# Plot a histogram of allele frequencies for one of the DNA smiulations
+makeAlleleFreqHist(DNA_N4800_geninds[[4]][[3]], 
+                   title="Simulated DNA (N4800) : 1,000 loci, mutation.rate=5e-6 (4 pops, high migration)")
+
+# %%% Resampling ----
+# Specify number of resampling replicates, to use for both marker types
+num_reps <- 5
+# Export relevant functions and variables to the cluster
+clusterExport(cl, varlist = c("getWildFreqs", "getAlleleCategories", "exSitu_Sample", 
+                              "exSitu_Resample", "parResample_genind", "num_reps",
+                              "MSAT_N4800_geninds", "DNA_N4800_geninds"))
+
+# MSAT 
+MSAT_N4800_resamplingArrays <- 
+  rapply(MSAT_N4800_geninds, parResample_genind, reps=num_reps, cluster=cl, how = "list")
+# DNA
+DNA_N4800_resamplingArrays <- 
+  rapply(DNA_N4800_geninds, parResample_genind, reps=num_reps, cluster=cl, how = "list")
+
+# Close cores
+stopCluster(cl)
+
+# %%% Summarize resampling results ----
+# Calculate 95% minimum sampling size, and the mean values of each category, for all scenarios
+# MSAT
+MSAT_N4800_min95_Means <- rapply(MSAT_N4800_resamplingArrays, resample_min95_mean, how = "list")
+MSAT_N4800_meanValues <- rapply(MSAT_N4800_resamplingArrays, resample_meanValues, how = "list")
+# DNA
+DNA_N4800_min95Values <- rapply(DNA_N4800_resamplingArrays, resample_min95_mean, how = "list")
+DNA_N4800_meanValues <- rapply(DNA_N4800_resamplingArrays, resample_meanValues, how = "list")
+
+# %%% Plotting ---- 
+# Pick plot colors (for all plots!), with transparency for values other than Total
+plotColors <- c("red","red4","darkorange3","coral","purple")
+
+# Plotting commands nested in invisible function, to prevent text from being printed
+# MSAT
+invisible(rapply(MSAT_N4800_resamplingArrays, resample_Plot, colors=plotColors))
+# DNA
+invisible(rapply(DNA_N4800_resamplingArrays, resample_Plot, colors=plotColors))
